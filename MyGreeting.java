383d057eba6355ceca19ffcbbe672418c62899a8
Manoharan Gurusamy <Mano.Gurusamy01@gmail.com>
2017-08-23 22:56:24 +0100

    First Commit for Lambda Functions

---
+++ MyGreeting.java
@@ -0,0 +1,17 @@
+public class MyGreeting {
+	public void greet(Greeting greeting){
+		greeting.perform();
+	}
+	public static void main(String[] args) {
+		Greeting myGreetFunction = () -> System.out.println("Hello World!");
+		Greeting ananymousFunction = new Greeting(){
+			public void perform(){
+				System.out.println("Hello Ananymous Function");
+			}
+		};
+		MyGreeting myGreeting = new MyGreeting();		
+		myGreeting.greet(myGreetFunction);
+		myGreeting.greet(ananymousFunction);
+	}
+}
+
